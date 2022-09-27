f401f5b7a581942cb31531c54824dd35c5c86840
Raghad Alzahrani <103121433+RaghadTurkiAlzahrani@users.noreply.github.com>
2022-09-27 11:19:39 +0300

    Add files via upload

---
+++ challange1.py
@@ -0,0 +1,19 @@
+# Py-challenge-1
+# <!-- Question 1:
+def My_Zeros():
+    R= [4, 5, 0, 0, 6, 7, 0, 1, 0, 5]
+    R.sort(key=lambda v: v != 0)
+    print(R)
+My_Zeros()
+    # print(My_List)
+    # j=0
+    # for i in range(r):
+    #     if R[i] !=0:
+    #         R[j],R[i]=R[i],R[j]
+    #         j+=1
+    #         My_Zeros()
+    # list.sort(My_List)
+    # print(My_List)
+    # My_List=[1,2,3,4,6,7,8,0,0,0]
+
+
