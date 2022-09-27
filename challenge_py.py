b8abc097d84ac35a6763b2102c8095d7a69978c0
n4if11 <72503341+n4if11@users.noreply.github.com>
2022-09-27 13:09:42 +0300

    Update challenge_py.py

--- challenge_py.py
+++ challenge_py.py
@@ -1,11 +1,10 @@
 
-lst=[1,3,5,0,5,3,0,55,0,9,0,4,2,-1]
+lst=[1,3,5,0,5,3,0,55,0,9,0,4,2,4]
 
-y=0
 def arrange(lst):
     
     for i in range(len(lst)):
-        if lst[i]<1:
+        if lst[i]==0:
             x=lst.pop(i)
             lst.append(x)
             
